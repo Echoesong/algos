7a8eb240fca75989b6035ff1a4b8af4be4546e0b
Joseph Felker <jayefelker@gmail.com>
2023-07-26 12:43:06 -0500

    palindrome number with slice

--- Python/palindrome_number.py
+++ Python/palindrome_number.py
@@ -1,10 +1,14 @@
 def isPalindrome(nums):
     y = str(nums)
-    for char in y:
-        print(char)
-    pass
+    reverse = y[::-1]
+    if y == reverse:
+        return True
+    else:
+        return False
 
 
 x = 121
 
 isPalindrome(x)
+
+# The above changes the number into a string, then reverses it by using Python's slice operation to iterate over the array backwards. then it's a simple matter of comparison
