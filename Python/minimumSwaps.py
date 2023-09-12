f04ebda0666c32115c65042692dda28f58b92a72
Joseph Felker <jayefelker@gmail.com>
2023-09-12 16:56:43 -0500

    refactor in progress

--- Python/minimumSwaps.py
+++ Python/minimumSwaps.py
@@ -4,17 +4,22 @@
 # for : if array[i] == i + 1, continue, else: for j in range(i + 1)
 
 
+# Brute force solution:
 def minimumSwaps(arr):
     swaps = 0
+    dictionary = {}
     for i, num in enumerate(arr):
         if num == i + 1:
             continue
         else:
-            for j in range(i + 1, len(arr)):
-                if arr[j] == i + 1:
-                    arr[i], arr[j] = arr[j], arr[i]
-                    swaps += 1
-                    print("arr at end of loop: ", arr)
+            # i + 1 = correct number, num = wrong number
+            if num in dictionary:
+                dictionary.update({num: i + 1})
+                arr[i] = dictionary[num]
+                swaps -= 1
+            else:
+                dictionary.update({num: None})
+                swaps += 1
     return swaps
 
 
@@ -22,4 +27,4 @@ arr0 = [1, 2, 3, 4]
 arr1 = [4, 3, 1, 2]
 arr2 = [7, 1, 3, 2, 4, 5, 6]  # Expected output: 5
 arr3 = [1, 3, 5, 2, 4, 6, 7]  # Expected output: 3
-print(minimumSwaps(arr3))
+print(minimumSwaps(arr2))
