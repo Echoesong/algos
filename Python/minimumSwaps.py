720e5879891c1db8cb87d1dcd2f3c6586dbb2140
Joseph Felker <jayefelker@gmail.com>
2023-09-13 13:56:00 -0500

    optimization in progress

--- Python/minimumSwaps.py
+++ Python/minimumSwaps.py
@@ -3,23 +3,24 @@
 # Brute force: Iterate over array to find element = i + 1, swap it with element at i position.
 # for : if array[i] == i + 1, continue, else: for j in range(i + 1)
 
+# Add all numbers to a dictionary where key = index and value = value
+# For each pair in the dictionary, if key != key + 1, then:
+#   append cache
 
-# Brute force solution:
+
 def minimumSwaps(arr):
     swaps = 0
     dictionary = {}
+
     for i, num in enumerate(arr):
-        if num == i + 1:
-            continue
-        else:
-            # i + 1 = correct number, num = wrong number
-            if num in dictionary:
-                dictionary.update({num: i + 1})
-                arr[i] = dictionary[num]
-                swaps -= 1
-            else:
-                dictionary.update({num: None})
-                swaps += 1
+        dictionary.update({i: num - 1})
+    print(dictionary)
+    for key in list(dictionary.keys()):
+        while not dictionary[key] == key:
+            cache = dictionary[key]
+            dictionary[key], dictionary[cache] = dictionary[cache], dictionary[key]
+        swaps += 1
+
     return swaps
 
 
