e5c6768e94012548180cec5e55b2b9e59c9a8867
Joseph Felker <jayefelker@gmail.com>
2023-09-14 16:58:23 -0500

    solution progress

--- Python/ransomeNote.py
+++ Python/ransomeNote.py
@@ -1,20 +1,49 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'checkMagazine' function below.
+#
+# The function accepts following parameters:
+#  1. STRING_ARRAY magazine
+#  2. STRING_ARRAY note
+#
+
+
 def checkMagazine(magazine, note):
+    magazineDict = {}
     noteDict = {}
-    for word in note.split():
-        noteDict.update({word: []})
-    magazineWords = magazine.split()
-    for word in noteDict:
-        if word in magazineWords:
-            print(magazineWords)
-            noteDict[word].append(True)
-            magazineWords.remove(word)
+    output = "Yes"
+    for word in magazine:
+        if word in magazineDict.values():
+            magazineDict[word] += 1
         else:
-            noteDict[word].append(False)
-    print(noteDict.values())
-    if False in noteDict.values():
-        print("No")
-    else:
-        print("Yes")
+            magazineDict.update({word: 1})
+    for word in note:
+        if word in noteDict:
+            noteDict[word] += 1
+        else:
+            noteDict.update({word: 1})
+    print(magazineDict, noteDict)
+    for word in noteDict:
+        if word in magazineDict:
+            if magazineDict[word] >= noteDict[word]:
+                magazineDict[word] -= 1
+                noteDict[word] -= 1
+                continue
+            else:
+                output = "No"
+        else:
+            output = "No"
+    print(output)
 
 
-checkMagazine("two times three is not four", "two times two is four")
+checkMagazine(
+    ["two", "times", "three", "is", "not", "four"],
+    ["two", "times", "two", "is", "four"],
+)
