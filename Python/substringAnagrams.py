8e03bf1b7a6712f04cac89085091fd4e42d66244
Joseph Felker <jayefelker@gmail.com>
2023-09-19 15:57:59 -0500

    final solution

--- Python/substringAnagrams.py
+++ Python/substringAnagrams.py
@@ -1,20 +1,25 @@
 def sherlockAndAnagrams(s):
+    import math
+
     dict = {}
     count = 0
     for i, char in enumerate(s):
         j = i + 1
-        while j <= len(s) - i:
-            subString = s[i, j]
-            subString.sort()
-            if dict[subString] == 0:
-                dict[subString] = 1
+        while j <= len(s):
+            subString = s[i:j]
+            sortedSubString = "".join(sorted(list(subString)))
+            if not sortedSubString in dict.keys():
+                dict[sortedSubString] = 1
             else:
-                dict[subString] += 1
+                dict[sortedSubString] += 1
             j += 1
     for key, val in dict.items():
+        print(dict.keys())
+        print(dict.values())
         if val > 1:
-            count += val * val(-1) / 2
-    return count
+            count += val * (val - 1) / 2
+        print("Count: ", count)
+    return math.floor(count)
 
 
 s = "mom"
