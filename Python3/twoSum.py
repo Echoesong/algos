277687cc7891586e3b05b70806b84aa30d68fb60
Echoesong <jayefelker@gmail.com>
2024-05-20 15:56:38 -0500

    returning to ol classic

--- Python3/twoSum.py
+++ Python3/twoSum.py
@@ -1,2 +1,19 @@
 def twoSum(nums, target):
-    pass
+    nums.sort()
+    point1 = 0
+    point2 = len(nums) - 1
+    diff = nums[point2] - nums[point1]
+    while not point1 == point2:
+        print("diff: ", diff)
+        if diff > target:
+            point2 -= 1
+        elif diff < target:
+            point1 += 1
+        else:
+            return [point1, point2]
+
+
+testArray = [3, 4, 5, 6]
+targetNum = 7
+
+print(twoSum(testArray, targetNum))
